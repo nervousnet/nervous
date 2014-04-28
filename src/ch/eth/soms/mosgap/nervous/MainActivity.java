2800543641159a9f21396a3a534a8b634821d18d
LucNaterop <luca.naterop@bluewin.ch>
2014-04-28 14:50:39 +0200

    Added Toggle Button to start/stop the service and a method to check if the service is running.

--- src/ch/eth/soms/mosgap/nervous/MainActivity.java
+++ src/ch/eth/soms/mosgap/nervous/MainActivity.java
@@ -2,6 +2,8 @@ package ch.eth.soms.mosgap.nervous;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -11,45 +13,30 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
 	private static final String DEBUG_TAG = "MainActivity";
 
-	private Button buttonStartService;
-	private Button buttonStopService;
-	private Button buttonSettings;
-	private Button buttonUpload;
-
+	private ToggleButton toggleButtonOnOff;
 	private TextView textStatus;
+	private Button buttonSettings;
+	private Button buttonUpload;	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		buttonStartService = (Button) findViewById(R.id.button_start_service);
-		buttonStopService = (Button) findViewById(R.id.button_stop_service);
+		
+		toggleButtonOnOff = (ToggleButton) findViewById(R.id.togglebutton);
+		textStatus = (TextView) findViewById(R.id.text_status);		
 		buttonSettings = (Button) findViewById(R.id.button_settings);
 		buttonUpload = (Button) findViewById(R.id.button_upload);
 		
-		textStatus = (TextView) findViewById(R.id.text_status);
-		
-
-		buttonStartService.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				startSensorService();
-			}
-		});
-		
-		buttonStopService.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				stopSensorService();
-			}
-		});
-
+		toggleButtonOnOff.setChecked(serviceRunning());
 	}
-
+	
 	public void startSensorService() {
 		// Schedule
 		AlarmManager scheduler = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
@@ -73,13 +60,33 @@ public class MainActivity extends Activity {
 
 		scheduler.cancel(scheduledIntent);
 		Log.d(DEBUG_TAG, "Service stopped");
-	
 	}
-
+	
+	public void onToggleClicked(View view) {
+	    boolean on = ((ToggleButton) view).isChecked();
+	    if (on) {
+	    	textStatus.setText("The service is running.");
+	    	startSensorService();
+	    } else {
+	    	textStatus.setText("The service is not running.");
+	    	stopSensorService();
+	    }
+	}
+	
+	private boolean serviceRunning() {
+	    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+	        if ("ch.eth.soms.mosgap.nervous.SensorService".equals(service.service.getClassName())) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
 }
