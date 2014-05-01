3ad035bdcbd86f24be534bdf199ed916db237022
GHrama <rsridhar@student.ethz.ch>
2014-05-01 23:28:40 +0200

    Added buttonExport to export sensorlog.txt to SD

--- src/ch/eth/soms/mosgap/nervous/MainActivity.java
+++ src/ch/eth/soms/mosgap/nervous/MainActivity.java
@@ -2,8 +2,6 @@ package ch.eth.soms.mosgap.nervous;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.ActivityManager;
-import android.app.ActivityManager.RunningServiceInfo;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -13,7 +11,20 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.ToggleButton;
+import android.os.Environment;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import android.content.Context;
+
+import java.io.FileWriter;
+import java.nio.channels.FileChannel;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -22,7 +33,7 @@ public class MainActivity extends Activity {
 	private ToggleButton toggleButtonOnOff;
 	private TextView textStatus;
 	private Button buttonSettings;
-	private Button buttonUpload;	
+	private Button buttonExport;	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +43,11 @@ public class MainActivity extends Activity {
 		toggleButtonOnOff = (ToggleButton) findViewById(R.id.togglebutton);
 		textStatus = (TextView) findViewById(R.id.text_status);		
 		buttonSettings = (Button) findViewById(R.id.button_settings);
-		buttonUpload = (Button) findViewById(R.id.button_upload);
+		buttonExport = (Button) findViewById(R.id.button_export);
 		
 		toggleButtonOnOff.setChecked(serviceRunning());
+		
+		 buttonExport.setOnClickListener(export_handler);
 	}
 	
 	public void startSensorService() {
@@ -72,6 +85,122 @@ public class MainActivity extends Activity {
 	    	stopSensorService();
 	    }
 	}
+	
+	 View.OnClickListener export_handler = new View.OnClickListener() {
+
+                    public void onClick(View v)
+                    {
+
+                        String state = Environment.getExternalStorageState();
+
+                        if (Environment.MEDIA_MOUNTED.equals(state))
+                        {
+                            Log.d(DEBUG_TAG, "SD card detected");
+
+
+
+                            stopSensorService();
+
+                            Log.d(DEBUG_TAG, "stopSensorService for file transfer");
+
+                            
+                            long TS = System.currentTimeMillis();
+                            StringBuilder stringBuilder = new StringBuilder();
+
+                            stringBuilder.append(TS);
+                            stringBuilder.append(".txt");
+
+                            String file_name = stringBuilder.toString();
+                            Log.d(DEBUG_TAG,file_name);
+                            Log.d(DEBUG_TAG, "file_name assigned to time");
+
+                            
+
+
+                            String sdCard=Environment.getExternalStorageDirectory().getAbsolutePath();
+
+
+
+
+                            File dir = new File (sdCard + "/nervous");
+                            dir.mkdirs();
+                            File file_ext = new File(dir,file_name);
+
+                            Log.d(DEBUG_TAG,"store location of new file");
+
+                          
+
+
+                            try
+                            {
+                                file_ext.createNewFile();
+                                Log.d(DEBUG_TAG, "Create file with file_name");
+
+                                File file = getBaseContext().getFileStreamPath("SensorLog.txt");
+
+
+                                if(file.exists())
+                                {
+                                    Log.d(DEBUG_TAG,"SensorLog.txt exists");
+                                    FileInputStream read_file = openFileInput("SensorLog.txt");
+
+                                    Log.d(DEBUG_TAG,"created Sensorlog.txt file obj read_file");
+                                    
+                                    InputStreamReader inputStreamReader = new InputStreamReader(read_file);
+                                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                                    StringBuilder sb = new StringBuilder();
+                                    
+                                    sb.append("Timestamp of export to SD : "+TS+"\n");
+                                    String line;
+                                    while ((line = bufferedReader.readLine()) != null) {
+                                        sb.append(line);
+                                    }
+                                    BufferedWriter bufWr = null;
+
+                                    bufWr = new BufferedWriter(new FileWriter(file_ext, false));
+
+
+
+                                    
+                                    bufWr.append(sb.toString());
+                                    inputStreamReader.close();
+                                    bufWr.close();
+                                    read_file.close();
+
+
+
+
+                                    
+                                    getApplicationContext().deleteFile("SensorLog.txt");
+                                    Log.d(DEBUG_TAG,"deleted sensorLog.txt");
+
+                                }
+                                Log.d(DEBUG_TAG,"done with file transfer");
+
+
+                            }
+
+                            catch(Exception e){
+
+                                e.printStackTrace();
+                            }
+
+
+                            startSensorService();
+                            Log.d(DEBUG_TAG, "startSensorService for file transfer");
+
+
+
+                        }
+                        else
+
+                        {
+                            Log.d(DEBUG_TAG, "No external storage detected(cannot copy file)");
+                            Toast.makeText(getApplicationContext(), "No external storage", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                };
 	
 	private boolean serviceRunning() {
 	    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
