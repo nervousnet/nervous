dd0c5e8be724e5e66998fbb6735ac604b70e4601
Fabian Tschopp <naibaf710@hotmail.com>
2014-08-24 21:13:32 +0200

    worker skeleton

--- router/router/src/ch/ethz/soms/nervous/router/network/SimpleUploadWorker.java
+++ router/router/src/ch/ethz/soms/nervous/router/network/SimpleUploadWorker.java
@@ -2,11 +2,14 @@ package ch.ethz.soms.nervous.router.network;
 
 import java.net.Socket;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SimpleUploadWorker extends ConcurrentSocketWorker {
 
 	Connection connection;
-	
+
 	public SimpleUploadWorker(Socket socket, Connection connection) {
 		this.connection = connection;
 	}
@@ -15,6 +18,17 @@ public class SimpleUploadWorker extends ConcurrentSocketWorker {
 	public void run() {
 		// TODO Basic data flow: Socket --> Connection
 		// TODO Retrieve data from socket, check/review/audit data, insert data into database
-	}
+		
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			for() {
+				stmt.
+			}
+			
+			int[] success = stmt.executeBatch();
+		} catch (SQLException e) {
 
+		}
+	}
 }
