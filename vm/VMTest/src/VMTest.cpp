bae7d3fc842f3afd7913e28e6be2eda0c80fb107
Fabian Tschopp <tschopfa@student.ethz.ch>
2014-09-02 00:53:19 +0200

    VM skeleton with C code for UUID generation, taken from libuuid (Linux kernel code).

---
+++ vm/VMTest/src/VMTest.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : VMTest.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description :
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "NervousVM Testing Project" << endl;
+	return 0;
+}
