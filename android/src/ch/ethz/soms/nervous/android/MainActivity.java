1aa684c9452ecdff09661d26a5593f3c675b8617
Fabian Tschopp <tschopfa@student.ethz.ch>
2014-09-29 01:25:42 +0200

    Updated to 0.5.3, R4 in order to avoid a battery drainage catastrophy (30 second sensor polling, 120 second uploading intervals again)

--- android/src/ch/ethz/soms/nervous/android/MainActivity.java
+++ android/src/ch/ethz/soms/nervous/android/MainActivity.java
@@ -82,10 +82,10 @@ public class MainActivity extends Activity {
 				PendingIntent.FLAG_UPDATE_CURRENT);
 
 		// 30 seconds
-		long sensorInterval = 10 * 1000;
+		long sensorInterval = 30 * 1000;
 
-		// 60 seconds
-		long uploadInterval = 30 * 1000;
+		// 120 seconds
+		long uploadInterval = 120 * 1000;
 
 		scheduler.setInexactRepeating(AlarmManager.RTC_WAKEUP,
 				System.currentTimeMillis(), sensorInterval,
