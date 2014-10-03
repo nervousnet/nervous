b2d334dd4fa4fe9e205270c139bd4ead08b91d83
Patrick Misteli <pat.misteli@gmail.com>
2014-10-03 15:35:59 +0200

    Sensor On/Off Toggles are now using text instead of images. Quality is independent of screen size now

--- android/src/ch/ethz/soms/nervous/android/SensorLoggingToggleActivity.java
+++ android/src/ch/ethz/soms/nervous/android/SensorLoggingToggleActivity.java
@@ -1,6 +1,7 @@
 package ch.ethz.soms.nervous.android;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +10,9 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,20 +35,28 @@ public class SensorLoggingToggleActivity extends Activity {
 			images_share[i] = R.raw.img_share_on;
 		}
 
-		CustomListAdapter adapter = new CustomListAdapter(SensorLoggingToggleActivity.this, sensorNames, images_log, images_share);
+		CustomListAdapter adapter = new CustomListAdapter(
+				SensorLoggingToggleActivity.this, sensorNames, images_log,
+				images_share);
 		listSensorLoggingToggle = (ListView) findViewById(R.id.list_SensorLoggingToggle);
 		listSensorLoggingToggle.setAdapter(adapter);
-		listSensorLoggingToggle.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Toast.makeText(SensorLoggingToggleActivity.this, "You Clicked at " + sensorNames[position], Toast.LENGTH_SHORT).show();
-			}
-		});
+		listSensorLoggingToggle
+				.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view,
+							int position, long id) {
+						Toast.makeText(SensorLoggingToggleActivity.this,
+								"You Clicked at " + sensorNames[position],
+								Toast.LENGTH_SHORT).show();
+					}
+				});
 
 	}
 
 	private String[] getSensorNames() {
-		String[] sensorList = { "Accelerometer", "Light", "Temperature", "Humidity", "Gyroscope", "Proximity", "Battery", "Atm. Pressure", "Magnetic", "Noise" };
+		String[] sensorList = { "Accelerometer", "Light", "Temperature",
+				"Humidity", "Gyroscope", "Proximity", "Battery",
+				"Atm. Pressure", "Magnetic", "Noise" };
 		return sensorList;
 	}
 
@@ -54,8 +65,10 @@ public class SensorLoggingToggleActivity extends Activity {
 		String[] sensorName;
 		private final Integer[] image_share, image_log;
 
-		public CustomListAdapter(Activity context, String[] i_sensorName, Integer[] images_log, Integer[] images_share) {
-			super(context, R.layout.sensor_logging_toggle_listitem, i_sensorName);
+		public CustomListAdapter(Activity context, String[] i_sensorName,
+				Integer[] images_log, Integer[] images_share) {
+			super(context, R.layout.sensor_logging_toggle_listitem,
+					i_sensorName);
 			this.context = context;
 			this.sensorName = i_sensorName;
 			this.image_log = images_log;
@@ -66,47 +79,49 @@ public class SensorLoggingToggleActivity extends Activity {
 		@Override
 		public View getView(int position, View view, ViewGroup parent) {
 			LayoutInflater inflater = context.getLayoutInflater();
-			View rowView = inflater.inflate(R.layout.sensor_logging_toggle_listitem, null, true);
-			TextView txtTitle = (TextView) rowView.findViewById(R.id.txt_SensorItem);
-			final ImageView imageView_Log = (ImageView) rowView.findViewById(R.id.img_log);
-			final ImageView imageView_Share = (ImageView) rowView.findViewById(R.id.img_share);
+			View rowView = inflater.inflate(
+					R.layout.sensor_logging_toggle_listitem, null, true);
+			TextView txtTitle = (TextView) rowView
+					.findViewById(R.id.txt_SensorItem);
+			final LinearLayout logLayoutButton = (LinearLayout) rowView
+					.findViewById(R.id.layoutButtonLog);
+			final LinearLayout shareLayoutButton = (LinearLayout) rowView
+					.findViewById(R.id.layoutButtonShare);
+			final TextView txtLogOnOff = (TextView) rowView
+					.findViewById(R.id.txt_LogOnOff);
+			final TextView txtShareOnOff = (TextView) rowView
+					.findViewById(R.id.txt_ShareOnOff);
+
 			txtTitle.setText(sensorName[position]);
-			imageView_Log.setImageResource(image_log[position]);
-			imageView_Share.setImageResource(image_share[position]);
+			logLayoutButton.setBackgroundColor(Color.GREEN);
+			txtLogOnOff.setText("ON");
+			shareLayoutButton.setBackgroundColor(Color.GREEN);
+			txtShareOnOff.setText("ON");
 
-			// Anroid cannot access getImageResource. It needs to be stored in a
-			// tag
-			imageView_Log.setTag(image_log[position]);
-			imageView_Share.setTag(image_share[position]);
-
-			imageView_Log.setOnClickListener(new OnClickListener() {
+			logLayoutButton.setOnClickListener(new OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
-					if ((Integer) imageView_Log.getTag() == R.raw.img_log_off) {
-						imageView_Log.setImageResource(R.raw.img_log_on);
-						imageView_Log.setTag(R.raw.img_log_on);
+					if (txtLogOnOff.getText().equals("ON")) {
+						txtLogOnOff.setText("OFF");
+						logLayoutButton.setBackgroundColor(Color.RED);
 					} else {
-						imageView_Log.setImageResource(R.raw.img_log_off);
-						imageView_Log.setTag(R.raw.img_log_off);
-						imageView_Share.setImageResource(R.raw.img_share_off);
-						imageView_Share.setTag(R.raw.img_share_off);
+						txtLogOnOff.setText("ON");
+						logLayoutButton.setBackgroundColor(Color.GREEN);
 					}
 				}
 			});
 
-			imageView_Share.setOnClickListener(new OnClickListener() {
+			shareLayoutButton.setOnClickListener(new OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
-					if ((Integer) imageView_Share.getTag() == R.raw.img_share_off) {
-						imageView_Share.setImageResource(R.raw.img_share_on);
-						imageView_Share.setTag(R.raw.img_share_on);
-						imageView_Log.setImageResource(R.raw.img_log_on);
-						imageView_Log.setTag(R.raw.img_log_on);
+					if (txtShareOnOff.getText().equals("ON")) {
+						txtShareOnOff.setText("OFF");
+						shareLayoutButton.setBackgroundColor(Color.RED);
 					} else {
-						imageView_Share.setImageResource(R.raw.img_share_off);
-						imageView_Share.setTag(R.raw.img_share_off);
+						txtShareOnOff.setText("ON");
+						shareLayoutButton.setBackgroundColor(Color.GREEN);
 					}
 				}
 			});
