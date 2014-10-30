937205feba9c90bf142e5ecb953f22543576d453
Fabian Tschopp <tschopfa@student.ethz.ch>
2014-10-30 20:16:21 +0100

    update on the performance test.

--- android/src/ch/ethz/soms/nervous/android/test/PerformanceTestTask2.java
+++ android/src/ch/ethz/soms/nervous/android/test/PerformanceTestTask2.java
@@ -6,11 +6,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.BatteryManager;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.widget.Toast;
 import ch.ethz.soms.nervous.android.sensors.SensorDesc;
 import ch.ethz.soms.nervous.android.sensors.SensorDescDummy;
 import ch.ethz.soms.nervous.nervousproto.SensorUploadProtos.SensorUpload.SensorData;
@@ -19,14 +23,14 @@ import ch.ethz.soms.nervous.vm.NervousVM;
 public class PerformanceTestTask2 extends AsyncTask<Void, Void, Void> {
 
 	private static final String LOG_TAG = PerformanceTestTask2.class.getSimpleName();
-	//private static final int TEST_COUNT = 24 * 60 * 60 / 5;
-	//private static final int TEST_SENSOR_COUNT = 20;
-	//private static final int REPETITIONS = 10;
-	
-	private static final int TEST_COUNT = 1000;
-	private static final int TEST_SENSOR_COUNT = 3;
-	private static final int REPETITIONS = 2;
-	
+	private static final int TEST_COUNT = 24 * 60 * 60 / 5;
+	private static final int TEST_SENSOR_COUNT = 20;
+	private static final int REPETITIONS = 10;
+
+	// private static final int TEST_COUNT = 1000;
+	// private static final int TEST_SENSOR_COUNT = 3;
+	// private static final int REPETITIONS = 2;
+
 	private Context context;
 
 	public PerformanceTestTask2(Context context) {
@@ -35,9 +39,14 @@ public class PerformanceTestTask2 extends AsyncTask<Void, Void, Void> {
 
 	@Override
 	protected Void doInBackground(Void... params) {
+		PowerManager powerManager = (PowerManager) context.getSystemService(Service.POWER_SERVICE);
+		WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "PerformanceTestWakelock");
+		wakeLock.acquire();
+
 		NervousVM vm = NervousVM.getInstance(context.getFilesDir());
 
 		PerformanceLog plog = new PerformanceLog();
+		plog.log("TEST USING: " + String.valueOf(TEST_COUNT) + "PER RUN, " + String.valueOf(TEST_SENSOR_COUNT) + " SENSORS, " + String.valueOf(REPETITIONS) + " REPETITIONS");
 
 		long tick, tock;
 		double time;
@@ -50,7 +59,7 @@ public class PerformanceTestTask2 extends AsyncTask<Void, Void, Void> {
 		// Test repetitions loop
 		for (int test = 0; test < REPETITIONS; test++) {
 
-			plog.log("RUN: " + String.valueOf(test) + " BATTERY BEFORE INSERT: " + String.valueOf(getBatteryLevel()) + " ms");
+			plog.log("RUN: " + String.valueOf(test) + " BATTERY BEFORE INSERT: " + String.valueOf(getBatteryLevel()));
 			// Test run (insert)
 			tick = System.currentTimeMillis();
 			for (int i = 0; i < TEST_COUNT; i++) {
@@ -63,7 +72,7 @@ public class PerformanceTestTask2 extends AsyncTask<Void, Void, Void> {
 			time = tock - tick;
 			plog.log("RUN: " + String.valueOf(test) + " INSERT TIME: " + String.valueOf(time) + " ms");
 
-			plog.log("RUN: " + String.valueOf(test) + " BATTERY BEFORE READ THROUGH: " + String.valueOf(getBatteryLevel()) + " ms");
+			plog.log("RUN: " + String.valueOf(test) + " BATTERY BEFORE READ THROUGH: " + String.valueOf(getBatteryLevel()));
 			// Test run (read through)
 			tick = System.currentTimeMillis();
 			for (int sid = 0; sid < TEST_SENSOR_COUNT; sid++) {
@@ -80,7 +89,7 @@ public class PerformanceTestTask2 extends AsyncTask<Void, Void, Void> {
 				storageSizeSum = storageSize[0] + storageSize[1];
 			}
 			plog.log("RUN: " + String.valueOf(test) + " STORAGE USAGE: " + String.valueOf(storageSizeSum) + " B");
-			plog.log("RUN: " + String.valueOf(test) + " BATTERY AFTER RUN: " + String.valueOf(getBatteryLevel()) + " ms");
+			plog.log("RUN: " + String.valueOf(test) + " BATTERY AFTER RUN: " + String.valueOf(getBatteryLevel()));
 
 			// Reset
 			for (int sid = 0; sid < TEST_SENSOR_COUNT; sid++) {
@@ -89,7 +98,7 @@ public class PerformanceTestTask2 extends AsyncTask<Void, Void, Void> {
 		}
 
 		plog.close();
-
+		wakeLock.release();
 		return null;
 	}
 
@@ -112,6 +121,7 @@ public class PerformanceTestTask2 extends AsyncTask<Void, Void, Void> {
 				bw.flush();
 			} catch (IOException e) {
 			}
+			// Toast.makeText(context, text, Toast.LENGTH_LONG).show();
 		}
 
 		public void close() {
