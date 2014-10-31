a3436c8abe38e5c800ae1b7dc626f2013704ae11
Fabian Tschopp <tschopfa@student.ethz.ch>
2014-10-31 20:10:44 +0100

    -NervousMap logic
    -VM performance Nexus 5

--- android/src/ch/ethz/soms/nervous/android/NervousMap.java
+++ android/src/ch/ethz/soms/nervous/android/NervousMap.java
@@ -1,10 +1,16 @@
 package ch.ethz.soms.nervous.android;
 
+import java.util.HashMap;
+
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
 
 import android.content.Context;
 
 public class NervousMap {
+
+	private HashMap<Integer, ITileSource> tileSources;
 
 	private Context context;
 	private MapView mapView;
@@ -19,5 +25,17 @@ public class NervousMap {
 	public MapView getMap() {
 		return mapView;
 
+	}
+
+	public void selectMapLayer(int mapLayer) {
+		if (mapLayer == -1) {
+			mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+		} else {
+			mapView.setTileSource(tileSources.get(mapLayer));
+		}
+	}
+
+	public void addMapLayer(int mapLayer, ITileSource tileSource) {
+		tileSources.put(mapLayer, tileSource);
 	}
 }
