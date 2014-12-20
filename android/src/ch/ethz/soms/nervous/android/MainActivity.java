9ce2b150af6d2d5930446db683203a399070b654
Fabian Tschopp <tschopfa@student.ethz.ch>
2014-12-20 15:47:41 +0100

    Project refactoring.

--- android/src/ch/ethz/soms/nervous/android/MainActivity.java
+++ android/src/ch/ethz/soms/nervous/android/MainActivity.java
@@ -46,31 +46,27 @@ import ch.ethz.soms.nervous.map.AssetsMbTileSource;
 import ch.ethz.soms.nervous.map.MapGraphLoader;
 import ch.ethz.soms.nervous.map.NervousMap;
 import android.widget.Toast;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
 	public static final String DEBUG_TAG = "MainActivity";
 
-	private TextView textStatus;
-	private ToggleButton buttonOnOff;
-	private Button buttonPerfTest;
-
 	private NervousMap nervousMap;
-
 	private boolean serviceRunning, menuButtonsShowing;
-
 	private ImageButton mainMenuButton;
-
 	private ImageView imgOverlay;
-
 	private RelativeLayout layoutMainMap;
-
 	private LinearLayout layoutExtraMenuButtonGroup;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Toolbar toolbar = (Toolbar) findViewById(R.id.fragment_toolbar);
+		this.setSupportActionBar(toolbar);
 
 		nervousMap = new NervousMap(getApplicationContext());
 
@@ -82,17 +78,6 @@ public class MainActivity extends Activity {
 		new MapGraphLoader(getApplicationContext(), "http://nervous.ethz.ch/app_data/map-sn.json", nervousMap, 0, 0).execute();
 
 		nervousMap.selectMapLayer(0);
-
-		textStatus = (TextView) findViewById(R.id.text_status);
-		buttonOnOff = (ToggleButton) findViewById(R.id.togglebutton);
-		buttonPerfTest = (Button) findViewById(R.id.perftestbutton);
-
-		buttonPerfTest.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				new PerformanceTestTask2(getApplicationContext()).execute();
-			}
-		});
 
 		menuButtonsShowing = false;
 		setupAnimations();
@@ -299,7 +284,6 @@ public class MainActivity extends Activity {
 
 		serviceRunning = true;
 		new ServiceInfo(getApplicationContext()).clean();
-		textStatus.setText("Service started");
 		Log.d(DEBUG_TAG, "Service started");
 	}
 
@@ -347,8 +331,6 @@ public class MainActivity extends Activity {
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-						buttonOnOff.setChecked(serviceRunning);
-						textStatus.setText(strBuf); // Runs on UI Thread
 					}
 				});
 			}
@@ -363,6 +345,8 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		TestQueries tq = new TestQueries(getApplicationContext(), getFilesDir());
+
 		Intent intent;
 		switch (item.getItemId()) {
 		case R.id.menu_IfThisThenThat:
@@ -374,49 +358,49 @@ public class MainActivity extends Activity {
 			startActivity(intent);
 			break;
 		case R.id.menu_TestQuery_Battery_MinBattery:
-			minBattery();
+			tq.minBattery();
 			break;
 		case R.id.menu_TestQuery_Battery_Bottom10:
-			bottom10Battery();
+			tq.bottom10Battery();
 			break;
 		case R.id.menu_TestQuery_Battery_Top10:
-			top10Battery();
+			tq.top10Battery();
 			break;
 		case R.id.menu_TestQuery_Battery_MaxBattery:
-			maxBattery();
+			tq.maxBattery();
 			break;
 		case R.id.menu_TestQuery_Light_MaxLight:
-			maxLight();
+			tq.maxLight();
 			break;
 		case R.id.menu_TestQuery_Light_MinLight:
-			minLight();
+			tq.minLight();
 			break;
 		case R.id.menu_TestQuery_Light_Top10Light:
-			top10Light();
+			tq.top10Light();
 			break;
 		case R.id.menu_TestQuery_Light_Bottom10Light:
-			bottom10Light();
+			tq.bottom10Light();
 			break;
 		case R.id.menu_TestQuery_Accelerometer_MaxAccAverage:
-			maxAverageAccelerometer();
+			tq.maxAverageAccelerometer();
 			break;
 		case R.id.menu_TestQuery_Accelerometer_MinAccAverage:
-			minAverageAccelerometer();
+			tq.minAverageAccelerometer();
 			break;
 		case R.id.menu_TestQuery_Proximity_MaxProx:
-			maxProximity();
+			tq.maxProximity();
 			break;
 		case R.id.menu_TestQuery_Proximity_MinProx:
-			minProximity();
+			tq.minProximity();
 			break;
 		case R.id.menu_TestQuery_Proximity_Top10Prox:
-			top10Proximity();
+			tq.top10Proximity();
 			break;
 		case R.id.menu_TestQuery_Proximity_Bottom10Prox:
-			bottom10Proximity();
+			tq.bottom10Proximity();
 			break;
 		case R.id.menu_TestQuery_Light_Prox_Kmean:
-			lightProxKMean();
+			tq.lightProxKMean();
 			break;
 		case R.id.menu_settings:
 			intent = new Intent(this, SettingsActivity.class);
@@ -428,192 +412,7 @@ public class MainActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void lightProxKMean() {
-		SensorQueriesMultipleSensors sq = new SensorQueriesMultipleSensors();
-		SensorQueriesLight sensorQ_Light = new SensorQueriesLight(1, Long.MAX_VALUE, getFilesDir());
-		SensorQueriesProximity sensorQ_Prox = new SensorQueriesProximity(1, Long.MAX_VALUE, getFilesDir());
-		ArrayList<Vector<Float>> res = sq.getKMeans(sensorQ_Light.getSensorDescriptorList(), sensorQ_Prox.getSensorDescriptorList());
-		toastToScreen(res.get(0).get(0) + "", false);
-	}
-
-	private void minLight() {
-		SensorQueriesLight sensorQ_Light2 = new SensorQueriesLight(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Light2.containsReadings()) {
-			SensorDescLight minLightDesc = sensorQ_Light2.getMinValue();
-			toastToScreen("Minimum Light: " + minLightDesc.getLight() + "\nat " + getDate(minLightDesc.getTimestamp()), true);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void maxLight() {
-		SensorQueriesLight sensorQ_Light = new SensorQueriesLight(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Light.containsReadings()) {
-			SensorDescLight maxLightDesc = sensorQ_Light.getMaxValue();
-			toastToScreen("Maximum Light: " + maxLightDesc.getLight() + "\nat " + getDate(maxLightDesc.getTimestamp()), true);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void bottom10Light() {
-		SensorQueriesLight sensorQ_Light = new SensorQueriesLight(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Light.containsReadings()) {
-			ArrayList<SensorDescLight> topKLightDesc = sensorQ_Light.getBottomK(10);
-			int i = 1;
-			Log.d(DEBUG_TAG, "Bottom 10 Light:");
-			for (SensorDescLight bat : topKLightDesc) {
-				Log.d(DEBUG_TAG, i++ + ": " + bat.getLight() + " Date: " + getDate(bat.getTimestamp()));
-			}
-			toastToScreen("Bottom 10 logged", false);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void top10Light() {
-		SensorQueriesLight sensorQ_Light = new SensorQueriesLight(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Light.containsReadings()) {
-			ArrayList<SensorDescLight> topKLightDesc = sensorQ_Light.getTopK(10);
-			int i = 1;
-			Log.d(DEBUG_TAG, "Top 10 Light:");
-			for (SensorDescLight light : topKLightDesc) {
-				Log.d(DEBUG_TAG, i++ + ": " + light.getLight() + " Date: " + getDate(light.getTimestamp()));
-			}
-			toastToScreen("Top 10 logged", false);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void minProximity() {
-		SensorQueriesProximity sensorQ_Proximity = new SensorQueriesProximity(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Proximity.containsReadings()) {
-			SensorDescProximity minProxDesc = sensorQ_Proximity.getMinValue();
-			toastToScreen("Minimum Proximity: " + minProxDesc.getProximity() + "\nat " + getDate(minProxDesc.getTimestamp()), true);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void maxProximity() {
-		SensorQueriesProximity sensorQ_Prox = new SensorQueriesProximity(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Prox.containsReadings()) {
-			SensorDescProximity maxProxDesc = sensorQ_Prox.getMaxValue();
-			toastToScreen("Maximum Prox: " + maxProxDesc.getProximity() + "\nat " + getDate(maxProxDesc.getTimestamp()), true);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void bottom10Proximity() {
-		SensorQueriesProximity sensorQ_Prox = new SensorQueriesProximity(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Prox.containsReadings()) {
-			ArrayList<SensorDescProximity> topKProxDesc = sensorQ_Prox.getBottomK(10);
-			int i = 1;
-			Log.d(DEBUG_TAG, "Bottom 10 Prox:");
-			for (SensorDescProximity proxDesc : topKProxDesc) {
-				Log.d(DEBUG_TAG, i++ + ": " + proxDesc.getProximity() + " Date: " + getDate(proxDesc.getTimestamp()));
-			}
-			toastToScreen("Bottom 10 logged", false);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void top10Proximity() {
-		SensorQueriesProximity sensorQ_Light = new SensorQueriesProximity(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Light.containsReadings()) {
-			ArrayList<SensorDescProximity> topKProxDesc = sensorQ_Light.getTopK(10);
-			int i = 1;
-			Log.d(DEBUG_TAG, "Top 10 Prox:");
-			for (SensorDescProximity proxDesc : topKProxDesc) {
-				Log.d(DEBUG_TAG, i++ + ": " + proxDesc.getProximity() + " Date: " + getDate(proxDesc.getTimestamp()));
-			}
-			toastToScreen("Top 10 logged", false);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void maxAverageAccelerometer() {
-		SensorQueriesAccelerometer sensorQ_Accel = new SensorQueriesAccelerometer(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Accel.containsReadings()) {
-			SensorDescAccelerometer maxAccAverageSensDesc = sensorQ_Accel.getMaxAverageValue();
-			toastToScreen("Maximum Accelerometer Average: \n x:" + maxAccAverageSensDesc.getAccX() + "\ny: " + maxAccAverageSensDesc.getAccY() + "\nz: " + maxAccAverageSensDesc.getAccZ() + "\nDate: " + getDate(maxAccAverageSensDesc.getTimestamp()), true);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void minAverageAccelerometer() {
-		SensorQueriesAccelerometer sensorQ_Accel = new SensorQueriesAccelerometer(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Accel.containsReadings()) {
-			SensorDescAccelerometer minAccAverageSensDesc = sensorQ_Accel.getMinAverageValue();
-			toastToScreen("Minimum Accelerometer Average: \n x:" + minAccAverageSensDesc.getAccX() + "\ny: " + minAccAverageSensDesc.getAccY() + "\nz: " + minAccAverageSensDesc.getAccZ() + "\nDate: " + getDate(minAccAverageSensDesc.getTimestamp()), true);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void maxBattery() {
-		SensorQueriesBattery sensorQ_Batteries2 = new SensorQueriesBattery(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Batteries2.containsReadings()) {
-			SensorDescBattery maxBatDesc = sensorQ_Batteries2.getMaxValue();
-			toastToScreen("Max Battery: " + maxBatDesc.getBatteryPercent() + "\nat " + getDate(maxBatDesc.getTimestamp()), true);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void minBattery() {
-		SensorQueriesBattery sensorQ_Batteries = new SensorQueriesBattery(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Batteries.containsReadings()) {
-			SensorDescBattery minBatDesc = sensorQ_Batteries.getMinValue();
-			toastToScreen("Minimum Battery: " + minBatDesc.getBatteryPercent() + "\nat " + getDate(minBatDesc.getTimestamp()), true);
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void bottom10Battery() {
-		SensorQueriesBattery sensorQ_Batteries = new SensorQueriesBattery(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Batteries.containsReadings()) {
-			ArrayList<SensorDescBattery> topKBatDesc = sensorQ_Batteries.getBottomK(10);
-			int i = 1;
-			Log.d(DEBUG_TAG, "Bottom 10 Bat:");
-			for (SensorDescBattery bat : topKBatDesc) {
-				toastToScreen("Bottom 10 logged", false);
-				Log.d(DEBUG_TAG, i++ + ": " + bat.getBatteryPercent() + " Date: " + getDate(bat.getTimestamp()));
-			}
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private void top10Battery() {
-		SensorQueriesBattery sensorQ_Batteries = new SensorQueriesBattery(1, Long.MAX_VALUE, getFilesDir());
-		if (sensorQ_Batteries.containsReadings()) {
-			ArrayList<SensorDescBattery> topKBatDesc = sensorQ_Batteries.getTopK(10);
-			int i = 1;
-			Log.d(DEBUG_TAG, "Top 10 Bat:");
-			for (SensorDescBattery bat : topKBatDesc) {
-				toastToScreen("Top 10 logged", false);
-				Log.d(DEBUG_TAG, i++ + ": " + bat.getBatteryPercent() + " Date: " + getDate(bat.getTimestamp()));
-			}
-		} else {
-			toastToScreen("No Data Found", false);
-		}
-	}
-
-	private String getDate(long time) {
-		Calendar cal = Calendar.getInstance(Locale.ENGLISH);
-		cal.setTimeInMillis(time);
-		String date = DateFormat.format("dd-MM-yyyy hh:mm:ss", cal).toString();
-		return date;
-	}
-
-	private void toastToScreen(String msg, boolean lengthLong) {
+	public void toastToScreen(String msg, boolean lengthLong) {
 
 		int toastLength = lengthLong ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT;
 		Toast.makeText(getApplicationContext(), msg, toastLength).show();
---
+++ android/src/ch/ethz/soms/nervous/android/TestQueries.java
@@ -0,0 +1,230 @@
+package ch.ethz.soms.nervous.android;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Vector;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.widget.Toast;
+import ch.ethz.soms.nervous.android.sensorQueries.SensorQueriesAccelerometer;
+import ch.ethz.soms.nervous.android.sensorQueries.SensorQueriesBattery;
+import ch.ethz.soms.nervous.android.sensorQueries.SensorQueriesLight;
+import ch.ethz.soms.nervous.android.sensorQueries.SensorQueriesMultipleSensors;
+import ch.ethz.soms.nervous.android.sensorQueries.SensorQueriesProximity;
+import ch.ethz.soms.nervous.android.sensors.SensorDescAccelerometer;
+import ch.ethz.soms.nervous.android.sensors.SensorDescBattery;
+import ch.ethz.soms.nervous.android.sensors.SensorDescLight;
+import ch.ethz.soms.nervous.android.sensors.SensorDescProximity;
+
+public class TestQueries {
+	
+	private static final String DEBUG_TAG = TestQueries.class.getSimpleName();
+
+	private Context context;
+	private File filesDir;
+	
+	public TestQueries(Context context, File filesDir)
+	{
+		this.context = context;
+		this.filesDir = filesDir;
+	}
+	
+	public void lightProxKMean() {
+		SensorQueriesMultipleSensors sq = new SensorQueriesMultipleSensors();
+		SensorQueriesLight sensorQ_Light = new SensorQueriesLight(1, Long.MAX_VALUE, getFilesDir());
+		SensorQueriesProximity sensorQ_Prox = new SensorQueriesProximity(1, Long.MAX_VALUE, getFilesDir());
+		ArrayList<Vector<Float>> res = sq.getKMeans(sensorQ_Light.getSensorDescriptorList(), sensorQ_Prox.getSensorDescriptorList());
+		toastToScreen(res.get(0).get(0) + "", false);
+	}
+
+	private File getFilesDir() {
+		return filesDir;
+	}
+
+	public void minLight() {
+		SensorQueriesLight sensorQ_Light2 = new SensorQueriesLight(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Light2.containsReadings()) {
+			SensorDescLight minLightDesc = sensorQ_Light2.getMinValue();
+			toastToScreen("Minimum Light: " + minLightDesc.getLight() + "\nat " + getDate(minLightDesc.getTimestamp()), true);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void maxLight() {
+		SensorQueriesLight sensorQ_Light = new SensorQueriesLight(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Light.containsReadings()) {
+			SensorDescLight maxLightDesc = sensorQ_Light.getMaxValue();
+			toastToScreen("Maximum Light: " + maxLightDesc.getLight() + "\nat " + getDate(maxLightDesc.getTimestamp()), true);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void bottom10Light() {
+		SensorQueriesLight sensorQ_Light = new SensorQueriesLight(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Light.containsReadings()) {
+			ArrayList<SensorDescLight> topKLightDesc = sensorQ_Light.getBottomK(10);
+			int i = 1;
+			Log.d(DEBUG_TAG, "Bottom 10 Light:");
+			for (SensorDescLight bat : topKLightDesc) {
+				Log.d(DEBUG_TAG, i++ + ": " + bat.getLight() + " Date: " + getDate(bat.getTimestamp()));
+			}
+			toastToScreen("Bottom 10 logged", false);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void top10Light() {
+		SensorQueriesLight sensorQ_Light = new SensorQueriesLight(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Light.containsReadings()) {
+			ArrayList<SensorDescLight> topKLightDesc = sensorQ_Light.getTopK(10);
+			int i = 1;
+			Log.d(DEBUG_TAG, "Top 10 Light:");
+			for (SensorDescLight light : topKLightDesc) {
+				Log.d(DEBUG_TAG, i++ + ": " + light.getLight() + " Date: " + getDate(light.getTimestamp()));
+			}
+			toastToScreen("Top 10 logged", false);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void minProximity() {
+		SensorQueriesProximity sensorQ_Proximity = new SensorQueriesProximity(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Proximity.containsReadings()) {
+			SensorDescProximity minProxDesc = sensorQ_Proximity.getMinValue();
+			toastToScreen("Minimum Proximity: " + minProxDesc.getProximity() + "\nat " + getDate(minProxDesc.getTimestamp()), true);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void maxProximity() {
+		SensorQueriesProximity sensorQ_Prox = new SensorQueriesProximity(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Prox.containsReadings()) {
+			SensorDescProximity maxProxDesc = sensorQ_Prox.getMaxValue();
+			toastToScreen("Maximum Prox: " + maxProxDesc.getProximity() + "\nat " + getDate(maxProxDesc.getTimestamp()), true);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void bottom10Proximity() {
+		SensorQueriesProximity sensorQ_Prox = new SensorQueriesProximity(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Prox.containsReadings()) {
+			ArrayList<SensorDescProximity> topKProxDesc = sensorQ_Prox.getBottomK(10);
+			int i = 1;
+			Log.d(DEBUG_TAG, "Bottom 10 Prox:");
+			for (SensorDescProximity proxDesc : topKProxDesc) {
+				Log.d(DEBUG_TAG, i++ + ": " + proxDesc.getProximity() + " Date: " + getDate(proxDesc.getTimestamp()));
+			}
+			toastToScreen("Bottom 10 logged", false);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void top10Proximity() {
+		SensorQueriesProximity sensorQ_Light = new SensorQueriesProximity(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Light.containsReadings()) {
+			ArrayList<SensorDescProximity> topKProxDesc = sensorQ_Light.getTopK(10);
+			int i = 1;
+			Log.d(DEBUG_TAG, "Top 10 Prox:");
+			for (SensorDescProximity proxDesc : topKProxDesc) {
+				Log.d(DEBUG_TAG, i++ + ": " + proxDesc.getProximity() + " Date: " + getDate(proxDesc.getTimestamp()));
+			}
+			toastToScreen("Top 10 logged", false);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void maxAverageAccelerometer() {
+		SensorQueriesAccelerometer sensorQ_Accel = new SensorQueriesAccelerometer(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Accel.containsReadings()) {
+			SensorDescAccelerometer maxAccAverageSensDesc = sensorQ_Accel.getMaxAverageValue();
+			toastToScreen("Maximum Accelerometer Average: \n x:" + maxAccAverageSensDesc.getAccX() + "\ny: " + maxAccAverageSensDesc.getAccY() + "\nz: " + maxAccAverageSensDesc.getAccZ() + "\nDate: " + getDate(maxAccAverageSensDesc.getTimestamp()), true);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void minAverageAccelerometer() {
+		SensorQueriesAccelerometer sensorQ_Accel = new SensorQueriesAccelerometer(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Accel.containsReadings()) {
+			SensorDescAccelerometer minAccAverageSensDesc = sensorQ_Accel.getMinAverageValue();
+			toastToScreen("Minimum Accelerometer Average: \n x:" + minAccAverageSensDesc.getAccX() + "\ny: " + minAccAverageSensDesc.getAccY() + "\nz: " + minAccAverageSensDesc.getAccZ() + "\nDate: " + getDate(minAccAverageSensDesc.getTimestamp()), true);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void maxBattery() {
+		SensorQueriesBattery sensorQ_Batteries2 = new SensorQueriesBattery(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Batteries2.containsReadings()) {
+			SensorDescBattery maxBatDesc = sensorQ_Batteries2.getMaxValue();
+			toastToScreen("Max Battery: " + maxBatDesc.getBatteryPercent() + "\nat " + getDate(maxBatDesc.getTimestamp()), true);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void minBattery() {
+		SensorQueriesBattery sensorQ_Batteries = new SensorQueriesBattery(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Batteries.containsReadings()) {
+			SensorDescBattery minBatDesc = sensorQ_Batteries.getMinValue();
+			toastToScreen("Minimum Battery: " + minBatDesc.getBatteryPercent() + "\nat " + getDate(minBatDesc.getTimestamp()), true);
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void bottom10Battery() {
+		SensorQueriesBattery sensorQ_Batteries = new SensorQueriesBattery(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Batteries.containsReadings()) {
+			ArrayList<SensorDescBattery> topKBatDesc = sensorQ_Batteries.getBottomK(10);
+			int i = 1;
+			Log.d(DEBUG_TAG, "Bottom 10 Bat:");
+			for (SensorDescBattery bat : topKBatDesc) {
+				toastToScreen("Bottom 10 logged", false);
+				Log.d(DEBUG_TAG, i++ + ": " + bat.getBatteryPercent() + " Date: " + getDate(bat.getTimestamp()));
+			}
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+
+	public void top10Battery() {
+		SensorQueriesBattery sensorQ_Batteries = new SensorQueriesBattery(1, Long.MAX_VALUE, getFilesDir());
+		if (sensorQ_Batteries.containsReadings()) {
+			ArrayList<SensorDescBattery> topKBatDesc = sensorQ_Batteries.getTopK(10);
+			int i = 1;
+			Log.d(DEBUG_TAG, "Top 10 Bat:");
+			for (SensorDescBattery bat : topKBatDesc) {
+				toastToScreen("Top 10 logged", false);
+				Log.d(DEBUG_TAG, i++ + ": " + bat.getBatteryPercent() + " Date: " + getDate(bat.getTimestamp()));
+			}
+		} else {
+			toastToScreen("No Data Found", false);
+		}
+	}
+	
+	public void toastToScreen(String msg, boolean lengthLong) {
+
+		int toastLength = lengthLong ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT;
+		Toast.makeText(context, msg, toastLength).show();
+	}
+	
+	private String getDate(long time) {
+		Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+		cal.setTimeInMillis(time);
+		String date = DateFormat.format("dd-MM-yyyy hh:mm:ss", cal).toString();
+		return date;
+	}
+}
