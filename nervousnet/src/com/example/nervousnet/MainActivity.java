25ea89dc6ea0aa555840abded674a5164c7301cb
Patrick Misteli <pat.misteli@gmail.com>
2015-04-15 16:54:34 +0200

    Main Interface is now linkable to other activities (See "TODO:" tag in MainAtivity.java)
    Readded animations
    Working with different screen dimensions
    
    Todo: Test on lower end devices

--- nervousnet/src/com/example/nervousnet/MainActivity.java
+++ nervousnet/src/com/example/nervousnet/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.nervousnet;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.Menu;
@@ -11,8 +12,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -42,11 +43,17 @@ public class MainActivity extends Activity {
 		btnColFreq = (ImageButton) findViewById(R.id.btn_collectionFrequency);
 
 		btnMain.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				vibrator.vibrate(vibDuration);
 				selectedActivity = 0;
+				animateButtonOutSelected(btnMain);
+				animateButtonOut(btnColFreq);
+				animateButtonOut(btnDataVis);
+				animateButtonOut(btnPrivacy);
 			}
+
 		});
 
 		btnPrivacy.setOnClickListener(new OnClickListener() {
@@ -54,6 +61,10 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				vibrator.vibrate(vibDuration);
 				selectedActivity = 1;
+				animateButtonOutSelected(btnPrivacy);
+				animateButtonOut(btnColFreq);
+				animateButtonOut(btnDataVis);
+				animateButtonOut(btnMain);
 			}
 		});
 
@@ -62,6 +73,10 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				vibrator.vibrate(vibDuration);
 				selectedActivity = 2;
+				animateButtonOutSelected(btnDataVis);
+				animateButtonOut(btnColFreq);
+				animateButtonOut(btnMain);
+				animateButtonOut(btnPrivacy);
 			}
 		});
 
@@ -70,6 +85,10 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				vibrator.vibrate(vibDuration);
 				selectedActivity = 3;
+				animateButtonOutSelected(btnColFreq);
+				animateButtonOut(btnMain);
+				animateButtonOut(btnDataVis);
+				animateButtonOut(btnPrivacy);
 			}
 		});
 
@@ -82,27 +101,6 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-
-		// TODO: Connect Activities here
-		int id = item.getItemId();
-		switch (id) {
-		case R.id.menu_main:
-			toastToScreen("Start Main Apps", false);
-			break;
-		case R.id.menu_visualizer:
-			toastToScreen("Start Visualizer", false);
-			break;
-		case R.id.menu_privacy:
-			toastToScreen("Start Privacy", false);
-			break;
-
-		default:
-			break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 
 	public void resetButtons(int maxX, int maxY) {
 		// Main Button
@@ -140,7 +138,7 @@ public class MainActivity extends Activity {
 		newY = (maxY * 0.8f) - (newH / 2);
 
 		resetButton(btnDataVis, newW, newH, newX, newY);
-		
+
 		// Collection Frequency button
 		scale = 0.2f;
 		w = maxX * scale;
@@ -164,8 +162,99 @@ public class MainActivity extends Activity {
 		params.height = newW;
 		params.width = newH;
 		btn.setLayoutParams(params);
+
+		// animate in
+		Random r = new Random();
+		ScaleAnimation s1, s2;
+		s1 = new ScaleAnimation(0, 1f, 0, 1f, newX + (newW / 2), newY
+				+ (newH / 2));
+		s1.setDuration(300);
+		s1.setStartOffset(250 + r.nextInt(250));
+
+		s2 = new ScaleAnimation(1.2f, 1, 1.2f, 1, newX + (newW / 2), newY
+				+ (newH / 2));
+		s2.setDuration(150);
+		s2.setStartOffset(s1.getStartOffset() + s1.getDuration());
+
+		AnimationSet sAll = new AnimationSet(false);
+		sAll.addAnimation(s1);
+		sAll.addAnimation(s2);
+		btn.startAnimation(sAll);
 	}
 
+	private void animateButtonOutSelected(ImageButton btn) {
+		ScaleAnimation s1, s2;
+		s1 = new ScaleAnimation(1, 2, 1, 2, btn.getX()
+				+ (btn.getWidth() / 2), btn.getY()
+				+ (btn.getHeight() / 2));
+		s1.setDuration(200);
+		
+		s2 = new ScaleAnimation(1, 0, 1, 0, btn.getX()
+				+ (btn.getWidth() / 2), btn.getY()
+				+ (btn.getHeight() / 2));
+		s2.setDuration(400);
+		s2.setStartOffset(s1.getStartOffset() + s1.getDuration());
+		
+		AnimationSet s = new AnimationSet(false);
+		s.addAnimation(s1);
+		s.addAnimation(s2);
+		s.setFillAfter(true);
+		s.setAnimationListener(new AnimationListener() {
+			
+			@Override
+			public void onAnimationStart(Animation animation) {
+			}
+			
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+			}
+			
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				// TODO: Connect Activities here
+				String msg = "";
+				Intent intent = null;
+				switch (selectedActivity) {
+				case 0:
+					msg = "Selected Main Apps";
+					break;
+				case 1:
+					msg = "Selected Privacy Settings";
+					intent = new Intent(MainActivity.this,
+							SensorLoggingToggleActivity.class);
+					break;
+				case 2:
+					msg = "Selected Data Visualization";
+					break;
+				case 3:
+					msg = "Selected Collection Frequency";
+					break;
+
+				default:
+					break;
+				}
+				if (intent == null) {
+					intent = new Intent(MainActivity.this,
+							SensorLoggingToggleActivity.class);
+				}
+//				toastToScreen(msg, false);
+				startActivity(intent);
+			}
+		});
+		btn.startAnimation(s);
+	}
+
+	
+	private void animateButtonOut(ImageButton btn) {
+		ScaleAnimation scaleAnimMinus;
+		scaleAnimMinus = new ScaleAnimation(1, 0, 1, 0, btn.getX()
+				+ (btn.getWidth() / 2), btn.getY()
+				+ (btn.getHeight() / 2));
+		scaleAnimMinus.setDuration(500);
+		scaleAnimMinus.setFillAfter(true);
+		btn.startAnimation(scaleAnimMinus);
+	}
+	
 	public void toastToScreen(String msg, boolean lengthLong) {
 
 		int toastLength = lengthLong ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT;
